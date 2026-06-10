bb7a57e49d07197a02bc204b792ac7edba45b726
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-10 12:30:17 +0530

    feat:add Header section

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header/Header";
+
+function App() {
+  return (
+    <div className="bg-gray-950">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import linkedInLogo from "../../assets/linkedIn.png";
+import github from "../../assets/github.png";
+export const Header = () => {
+  return (
+    <header className="w-full flex-1 text-white p-5 flex justify-between items-center">
+      {/* name/logo */}
+      <div className="flex items-baseline">
+        <h1 className="text-3xl font-bold">Jenith</h1>
+        <h2 className="text-2xl font-bold   ">Panchal</h2>
+      </div>
+
+      {/* navidgations */}
+      <nav className="flex gap-5 text-xl">
+        <a href="" className="hover:font-medium">
+          Home
+        </a>
+        <a href="" className="hover:font-medium">
+          About
+        </a>
+        <a href="" className="hover:font-medium">
+          Contact
+        </a>
+      </nav>
+
+      {/* links */}
+      <nav className="flex gap-5">
+        <a href="">
+          <img src={linkedInLogo} alt="LinkedIn Logo" className="h-10 w-10 " />
+        </a>
+        <a href="">
+          <img src={github} alt="LinkedIn Logo" className="h-10 w-10 " />
+        </a>
+      </nav>
+    </header>
+  );
+};

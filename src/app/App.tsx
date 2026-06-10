030333710e4ed4b62188a935d9a6349564ac63aa
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-10 13:43:59 +0530

    feat:add footer section

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { About } from "../components/About/About";
 import { Contact } from "../components/Contact/Contact";
+import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import { Home } from "../components/Home/Home";
 import { Projects } from "../components/Projects/Projects";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import linkedInLogo from "../../assets/linkedIn.png";
+import github from "../../assets/github.png";
+
+export const Footer = () => {
+  return (
+    <footer className="w-full flex-1 text-white p-5 px-20 flex justify-between items-center">
+      {/* name/logo */}
+      <div className="flex items-baseline">
+        <h1 className="text-3xl font-bold">Jenith</h1>
+        <h2 className="text-2xl font-bold   ">Panchal</h2>
+      </div>
+
+      {/* links */}
+      <nav className="flex gap-5">
+        <a href="">
+          <img src={linkedInLogo} alt="LinkedIn Logo" className="h-10 w-10 " />
+        </a>
+        <a href="">
+          <img src={github} alt="LinkedIn Logo" className="h-10 w-10 " />
+        </a>
+      </nav>
+    </footer>
+  );
+};
